7e391773598f41376d2068dfbee245b1e19b28c7
Sakriya777 <sakriya33@gmail.com>
2021-02-08 07:23:14 +0545

    from friday

--- app/src/main/java/com/example/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/scorekeeper/MainActivity.java
@@ -10,30 +10,87 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView PointsA, PointsB;
-    private Button Add3A, Add2A, Add1A, Add3B, Add2B, Add1B;
+    private TextView pointsA, pointsB;
+    private Button add3A, add2A, add1A, add3B, add2B, add1B;
+    private Button resetPoint;
+    private int teamAScore = 0;
+    private int teamBScore = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        PointsA = findViewById(R.id.PointA);
-        PointsB = findViewById(R.id.PointB);
+        pointsA = findViewById(R.id.PointA);
+        pointsB = findViewById(R.id.PointB);
 
-        Add1A = findViewById(R.id.button1A);
-        Add2A = findViewById(R.id.button2A);
-        Add3A = findViewById(R.id.button3A);
+        add1A = findViewById(R.id.button1A);
+        add2A = findViewById(R.id.button2A);
+        add3A = findViewById(R.id.button3A);
 
-        Add1B = findViewById(R.id.button1B);
-        Add2B = findViewById(R.id.button2B);
-        Add3B = findViewById(R.id.button3B);
+        add1B = findViewById(R.id.button1B);
+        add2B = findViewById(R.id.button2B);
+        add3B = findViewById(R.id.button3B);
 
+        resetPoint = findViewById(R.id.resetpoint);
 
-//        button3.setOnClickListner(new View.OnClickListener(){
-//            @Override
-//            public void onClick(View v){
-//                Toast.makeText(MainActivity.this, "Button CLicked", Toast.LENGTH_LONG.show());
-//            }
-//        });
+        add3A.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                teamAScore+=3;
+                DisplayForTeamA(teamAScore);
+            }
+        });
+        add2A.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                teamAScore+=2;
+                DisplayForTeamA(teamAScore);
+            }
+        });
+        add1A.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                teamAScore+=1;
+                DisplayForTeamA(teamAScore);
+            }
+        });
+        add3B.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                teamBScore+=3;
+                DisplayForTeamB(teamBScore);
+            }
+        });
+        add2B.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                teamBScore+=2;
+                DisplayForTeamB(teamBScore);
+            }
+        });
+        add2B.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                teamBScore+=1;
+                DisplayForTeamB(teamBScore);
+            }
+        });
+        resetPoint.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                teamBScore=0;
+                teamAScore=0;
+                DisplayForTeamB(teamBScore);
+                DisplayForTeamA(teamAScore);
+            }
+        });
+    }
+
+    private void DisplayForTeamB(int score) {
+        pointsB.setText(""+score);
+    }
+
+    private void DisplayForTeamA(int score) {
+        pointsA.setText(""+score);
     }
 }
